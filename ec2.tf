13f72e63ccbf630e13487c1129b92d469bbc5173
mojasim <mrjasim87@gmail.com>
2026-04-04 12:09:26 +0530

    Refactor Terraform EC2 configuration for improved readability and organization

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
 # AWS Region
 provider "aws" {
-    region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 # AWS SSH Key Pair
 resource "aws_key_pair" "my_key_pair" {
-    key_name   = "my-key-pair"
-    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILrsqpUaHSKd8a4xDXm6dBuVbr6SnshKpUkNGNvAiJMB mo-jasim@Mo-Jasim"
+  key_name   = "my-key-pair"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILrsqpUaHSKd8a4xDXm6dBuVbr6SnshKpUkNGNvAiJMB mo-jasim@Mo-Jasim"
 }
 
 # VPS ID
@@ -14,15 +14,15 @@ resource "aws_default_vpc" "default" {}
 
 # Security Group 
 resource "aws_security_group" "my_security_group" {
-    name        = "my-security-group"
-    description = "Security group for default VPC"
-    vpc_id      = aws_default_vpc.default.id
+  name        = "my-security-group"
+  description = "Security group for default VPC"
+  vpc_id      = aws_default_vpc.default.id
 }
 
 # SSH Port
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.my_security_group.id
-  cidr_ipv4 = aws_default_vpc.default.cidr_block
+  cidr_ipv4         = aws_default_vpc.default.cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
@@ -31,7 +31,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 # Inbound Port Rules
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.my_security_group.id
-  cidr_ipv4 = aws_default_vpc.default.cidr_block
+  cidr_ipv4         = aws_default_vpc.default.cidr_block
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
@@ -46,6 +46,8 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
 
 # EC2 Instance
 resource "aws_instance" "my_instance" {
+  # count = 2 # Create 2 instances
+  # instance_state = "stopped" # Stop the instance
   ami           = "ami-05d2d839d4f73aafb"
   instance_type = "t3.nano"
 
@@ -53,7 +55,7 @@ resource "aws_instance" "my_instance" {
 
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
 
-# EBS Storage
+  # EBS Storage
   root_block_device {
     volume_size = 10
     volume_type = "gp3"
@@ -62,4 +64,11 @@ resource "aws_instance" "my_instance" {
   tags = {
     Name = "terra-auto-server"
   }
+}
+
+# Instance State
+resource "aws_ec2_instance_state" "my_instance_state" {
+  for_each    = aws_instance.my_instance
+  instance_id = aws_instance.my_instance.id
+  state       = "stopped"
 }
