8059a1a6ed6620cd51b799f56cebef3ab5df0b33
mo-jasim <mrjasim87@gmail.com>
2026-04-15 17:29:56 +0800

    Main tf modified

--- aws-temlate/modules/s3/main.tf
+++ aws-temlate/modules/s3/main.tf
@@ -1,9 +1,8 @@
 resource aws_s3_bucket my_bucket {
-    bucket = "${var.aws_s3_bucket_name}-${var.env}-${count.index}"
-
     count = var.bucket_count
+    bucket = "${var.aws_s3_bucket_name}-${count.index + 1}"
 
     tags = {
-      Name = "${var.aws_s3_bucket_name}-${count.index}"
+      Name = "${var.aws_s3_bucket_name}-${count.index + 1}"
     }
 }
